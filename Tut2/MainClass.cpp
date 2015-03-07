32a5920960449663a273f0e4c16fc906a0cf5ce1
kev560 <singhkevin132@gmail.com>
2015-03-07 16:07:18 +0200

    Update to Main

--- Tut2/MainClass.cpp
+++ Tut2/MainClass.cpp
@@ -26,6 +26,8 @@ int main()
 	cout << "Enter denominator for fraction 2 : ";
 	cin >> d2;
 
+	cout << "\n";
+
 	obj.setNume(n1, n2);
 	obj.setDen(d1, d2);
 
@@ -38,5 +40,12 @@ int main()
 	obj.mult();
 	obj.div();
 
+	cout << "Addition: " << obj.getNume1() << "/" << obj.getDen1() << " + " << obj.getNume2() << "/" << obj.getDen2() << " = " << obj.add()<< "\n\n";
+	cout << "Subtraction: " << obj.getNume1() << "/" << obj.getDen1() << " - " << obj.getNume2() << "/" << obj.getDen2() << " = " << obj.sub() << "\n\n";
+	cout << "Multiplication: " << obj.getNume1() << "/" << obj.getDen1() << " x " << obj.getNume2() << "/" << obj.getDen2() << " = " << obj.mult() << "\n\n";
+	cout << "Division: " << obj.getNume1() << "/" << obj.getDen1() << " / " << obj.getNume2() << "/" << obj.getDen2() << " = " << obj.div() << "\n\n";
 
+	system("pause");
+
+	return 0;
 };
